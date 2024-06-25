9ea4ce64a418e808b1077ebe01a3435e9d3ae47b
Sami Al-Dury <git@aldury.com>
2024-06-25 10:56:43 +0200

    feat: add `yazi`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -83,6 +83,16 @@ end
 # shell
 set -gx SHELL (command -s fish)
 
+# yazi
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 # zoxide
 zoxide init fish | source
 
