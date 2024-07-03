f877e91384e45f9667ce8a687b3dc0f82525ee3d
Sami Al-Dury <git@aldury.com>
2024-07-03 21:08:09 +0200

    feat(nvim): include hidden files in telescope search

--- .config/nvim/lua/sami/plugins/telescope.lua
+++ .config/nvim/lua/sami/plugins/telescope.lua
@@ -17,7 +17,19 @@ return {
     local actions = require 'telescope.actions'
 
     telescope.setup {
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+        grep_string = {
+          additional_args = { '--hidden' },
+        },
+        live_grep = {
+          additional_args = { '--hidden' },
+        },
+      },
       defaults = {
+        file_ignore_patterns = { 'node_modules', '.git' },
         sorting_strategy = 'ascending',
         layout_strategy = 'horizontal',
         layout_config = { prompt_position = 'top' },
@@ -45,12 +57,15 @@ return {
     vim.keymap.set('n', '<leader>sH', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>sh', function()
-      builtin.find_files { hidden = true }
-    end, { desc = '[S]earch [F]iles including [H]idden' })
+    -- vim.keymap.set('n', '<leader>sF', function()
+    --   builtin.find_files { hidden = true }
+    -- end, { desc = '[S]earch [F]iles including [H]idden' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    -- vim.keymap.set('n', '<leader>sG', function()
+    --   builtin.live_grep { hidden = true }
+    -- end, { desc = '[S]earch by [G]rep including [H]idden' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
