1ddd3800d9fc1820dfea96642872372d2b937953
Sami Al-Dury <git@aldury.com>
2024-09-11 09:31:09 +0200

    add conform stop after first hint

--- .config/nvim/lua/sami/plugins/format.lua
+++ .config/nvim/lua/sami/plugins/format.lua
@@ -46,6 +46,8 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        -- Or use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
 
         javascript = { 'prettierd' },
         javascriptreact = { 'prettierd' },
