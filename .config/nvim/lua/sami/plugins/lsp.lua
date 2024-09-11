b01dda6249fc20383f53627e923fb5807a513b51
Sami Al-Dury <git@aldury.com>
2024-09-11 09:29:05 +0200

    fix lsp error

--- .config/nvim/lua/sami/plugins/lsp.lua
+++ .config/nvim/lua/sami/plugins/lsp.lua
@@ -13,7 +13,17 @@ return {
 
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
-    { 'folke/lazydev.nvim', ft = 'lua', opts = {} },
+    {
+      'folke/lazydev.nvim',
+      ft = 'lua',
+      opts = {
+        library = {
+          -- Load luvit types when the `vim.uv` word is found
+          { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        },
+      },
+    },
+    { 'Bilal2453/luvit-meta', lazy = true },
     {
       'pmizio/typescript-tools.nvim',
       dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
