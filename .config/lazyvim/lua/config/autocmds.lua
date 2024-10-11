a100727955629af0e2a17e954c408c5059f5b114
Sami Al-Dury <git@aldury.com>
2024-10-11 18:32:39 +0200

    lazyvim: go filetype augroup

--- .config/lazyvim/lua/config/autocmds.lua
+++ .config/lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,21 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Go
+local goAuGroup = vim.api.nvim_create_augroup("goFileType", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  group = goAuGroup,
+  callback = function()
+    vim.opt_local.list = false
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+
+    vim.keymap.set("n", "<leader>E", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", {
+      desc = "Insert error handling",
+      buffer = 0,
+    })
+  end,
+})
