3743f0be0e0cbe0217b1484e8395203f340b12fb
Sami Al-Dury <git@aldury.com>
2024-10-11 21:16:00 +0200

    lazyvim: some more keymas

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -2,6 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Override some default keymaps
+vim.keymap.del("n", "<leader>|")
+vim.keymap.set("n", "<leader>\\", "<C-W>v", { desc = "Split Window Right", remap = true })
+
+-- Move to end of line with alt-l
+vim.keymap.set("n", "<A-l>", "$")
+-- Move to start of line with alt-h
+vim.keymap.set("n", "<A-h>", "^")
+
 -- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
