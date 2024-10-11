9eb93f7c03b6207e4f56df90244f152a1c89e1ce
Sami Al-Dury <git@aldury.com>
2024-10-11 18:38:17 +0200

    lazyvim: support tailwind

--- .config/lazyvim/lua/config/lazy.lua
+++ .config/lazyvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- import/override with your plugins
     { import = "plugins" },
   },
