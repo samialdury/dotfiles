a8cc75624a26ec3da3f472f524b15888f25cc4a2
Sami Al-Dury <git@aldury.com>
2024-10-15 10:31:52 +0200

    keep the cursor in middle when scrolling

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,6 +11,10 @@ vim.keymap.set("n", "<A-l>", "$")
 -- Move to start of line with alt-h
 vim.keymap.set("n", "<A-h>", "^")
 
+-- Keep the cursor in the middle when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
