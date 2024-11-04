866b1405d48081073de665ef873accd1adff48d5
Sami Al-Dury <git@aldury.com>
2024-11-04 15:49:53 +0100

    nvim(telescope): ignore vendor & node module directories

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "^vendor/", "^node_modules/", "^public/vendor/" },
+      },
+    },
+  },
+}
