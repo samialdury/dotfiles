20f67d413055b4132c110dfebc4b7027c9914d8f
Sami Al-Dury <git@aldury.com>
2025-01-01 18:07:39 +0100

    nvim: plugins in one file

---
+++ .config/nvim/lua/plugins/all.lua
@@ -0,0 +1,168 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+      },
+    },
+  },
+  {
+    "mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pint",
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "^vendor/", "^node_modules/", "^public/vendor/" },
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        ["terraform-vars"] = false,
+        env = false,
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+            return false
+          end
+          return true
+        end,
+        conf = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^secrets*") then
+            return false
+          end
+          return true
+        end,
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        -- PHP
+        php = { "pint" },
+        -- Terraform (OpenTofu)
+        terraform = { "tofu_fmt" },
+        tf = { "tofu_fmt" },
+        ["terraform-vars"] = { "tofu_fmt" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+  {
+    "stevearc/oil.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      local oil = require("oil")
+
+      oil.setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          is_always_hidden = function(name)
+            return name == ".." or name == ".git"
+          end,
+        },
+      })
+
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      -- vim.keymap.set("n", "<leader>-", oil.toggle_float, { desc = "Toggle oil float" })
+    end,
+  },
+  {
+    "ThePrimeagen/harpoon",
+    keys = function()
+      local keys = {
+        {
+          "<leader>a",
+          function()
+            require("harpoon"):list():add()
+            Snacks.notify.info("File added", { title = "Harpoon" })
+          end,
+          desc = "Add file to Harpoon",
+        },
+        {
+          "<C-e>",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      local arr = { "h", "j", "k", "l" }
+
+      for i, key in ipairs(arr) do
+        table.insert(keys, {
+          "<M-" .. key .. ">",
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  },
+  {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
