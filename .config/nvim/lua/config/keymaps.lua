71908ee7dd6183946d634b7f3e9ae0c28992d1e5
Sami Al-Dury <git@aldury.com>
2025-11-15 10:30:31 +0100

    nvim: remove vscode specific keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,38 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-if vim.g.vscode then
-  local vscode = require("vscode")
-  vim.keymap.set("n", "<leader>ff", function()
-    vscode.action("find-it-faster.findFiles")
-  end)
-  vim.keymap.set("n", "<leader>/", function()
-    vscode.action("find-it-faster.findWithinFiles")
-  end)
-  vim.keymap.set("n", "<leader>ca", function()
-    vscode.action("editor.action.sourceAction")
-  end)
-  -- harpoon keymaps
-  vim.keymap.set("n", "<leader>a", function()
-    vscode.action("vscode-harpoon.addEditor")
-  end)
-  vim.keymap.set("n", "<C-e>", function()
-    vscode.action("vscode-harpoon.editEditors")
-  end)
-  vim.keymap.set("n", "<M-h>", function()
-    vscode.action("vscode-harpoon.gotoEditor1")
-  end)
-  vim.keymap.set("n", "<M-j>", function()
-    vscode.action("vscode-harpoon.gotoEditor2")
-  end)
-  vim.keymap.set("n", "<M-k>", function()
-    vscode.action("vscode-harpoon.gotoEditor3")
-  end)
-  vim.keymap.set("n", "<M-l>", function()
-    vscode.action("vscode-harpoon.gotoEditor4")
-  end)
-end
-
 -- Move Lines
 -- vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 -- vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
@@ -43,9 +11,7 @@ vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=g
 vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
 -- Override some default keymaps
-if not vim.g.vscode then
-  vim.keymap.del("n", "<leader>|")
-end
+vim.keymap.del("n", "<leader>|")
 vim.keymap.set("n", "<leader>\\", "<C-W>v", { desc = "Split Window Right", remap = true })
 
 -- Move to end of line with alt-l
