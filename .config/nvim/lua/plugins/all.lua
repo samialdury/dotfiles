f9a7bcdb79721965b2cbe369867fe59d666427d2
Sami Al-Dury <git@aldury.com>
2025-11-15 10:27:57 +0100

    nvim: remove copilot

--- .config/nvim/lua/plugins/all.lua
+++ .config/nvim/lua/plugins/all.lua
@@ -72,44 +72,6 @@ return {
     },
   },
   {
-    "zbirenbaum/copilot.lua",
-    enabled = false,
-    opts = function()
-      Snacks.toggle({
-        name = "Copilot Completion",
-        get = function()
-          return not require("copilot.client").is_disabled()
-        end,
-        set = function(state)
-          if state then
-            require("copilot.command").enable()
-          else
-            require("copilot.command").disable()
-          end
-        end,
-      }):map("<leader>ct")
-
-      return {
-        filetypes = {
-          ["terraform-vars"] = false,
-          env = false,
-          sh = function()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-              return false
-            end
-            return true
-          end,
-          conf = function()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^secrets*") then
-              return false
-            end
-            return true
-          end,
-        },
-      }
-    end,
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
