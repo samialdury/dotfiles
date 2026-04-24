969fdb3280e81dabda02f6ae0f849976230053cf
Sami Al-Dury <git@aldury.com>
2026-04-24 11:40:16 +0200

    nvim: handle biome/prettier in conform

--- .config/nvim/lua/plugins/all.lua
+++ .config/nvim/lua/plugins/all.lua
@@ -1,3 +1,30 @@
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+  if has_biome_lsp then
+    return {}
+  end
+  local has_prettier = vim.fs.find({
+    -- https://prettier.io/docs/en/configuration.html
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+  if has_prettier then
+    return { "prettier" }
+  end
+  return { "biome" }
+end
+
 return {
   {
     "folke/tokyonight.nvim",
@@ -79,6 +106,12 @@ return {
         terraform = { "tofu_fmt" },
         tf = { "tofu_fmt" },
         ["terraform-vars"] = { "tofu_fmt" },
+        javascript = biome_lsp_or_prettier,
+        typescript = biome_lsp_or_prettier,
+        javascriptreact = biome_lsp_or_prettier,
+        typescriptreact = biome_lsp_or_prettier,
+        json = biome_lsp_or_prettier,
+        jsonc = biome_lsp_or_prettier,
       },
     },
   },
