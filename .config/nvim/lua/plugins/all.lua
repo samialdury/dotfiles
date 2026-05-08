5d4a6fec519af56589108c87807bb8ee8e94e662
Sami Al-Dury <git@aldury.com>
2026-05-08 15:49:02 +0200

    nvim: remove telescope ignore overrides

--- .config/nvim/lua/plugins/all.lua
+++ .config/nvim/lua/plugins/all.lua
@@ -94,14 +94,6 @@ return {
     },
   },
   {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "^vendor/", "^node_modules/", "^public/vendor/" },
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
